387b71c49c6deaa60c4ae41e5b5ec61d77352288
olsonem <olsonem@seattleu.edu>
2019-03-26 12:35:22 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,52 @@
+document.addEventListener('DOMContentLoaded', function (e) {
+    document.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        let monthInput = document.querySelector('#month').value
+        let yearInput = document.querySelector('#year').value
+        let eligibility=calculateEligibility(monthInput,yearInput)
+        console.log('eligible',eligibility)
+        document.querySelector('#eligibility').innerHTML=eligibility
+    })
+})
+
+function calculateEligibility(inputMonth, inputYear) {
+    let months = ['aug', 'sep', 'oct', 'nov', 'dec', 'jan', 'feb', 'mar', 'apr', 'may', 'jun',
+        'jul'
+    ]
+    // let inputMonth = 'jun'
+    // let inputYear = 2012
+    let lookupKey = '' + inputYear + inputMonth
+    let currentYear = new Date().getFullYear()
+
+    let minYear = currentYear - 19
+    let maxYear = minYear + 15
+    console.log(minYear, maxYear)
+
+
+
+    let data = {}
+
+    let uAge = 19
+    let counter = 0
+    for (let i = minYear; i <= maxYear; i++) {
+        for (let j = 0; j < months.length; j++) {
+
+            let year = j < 5 ? i : (i + 1)
+            let key = '' + year + months[j]
+
+            if (uAge > 4) {
+                data[key] = 'U' + uAge
+                counter += 1
+                if (counter === 12) {
+                    uAge -= 1
+                    counter = 0
+                }
+            }
+        }
+    }
+    console.log(data)
+
+    console.log("eligibility: ", data[lookupKey]) 
+    return data[lookupKey]
+}
